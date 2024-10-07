7de83208bf32e3f16aa89ac4fa5e1a5c3b4188f0
bkisly <b.kisly@gmail.com>
2024-10-07 22:02:30 +0200

    Created basic projects structure

--- src/Saver.AppHost/Program.cs
+++ src/Saver.AppHost/Program.cs
@@ -1,3 +1,15 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddProject<Projects.Saver_Gateway>("saver-gateway");
+
+builder.AddProject<Projects.Saver_IdentityService>("saver-identityservice");
+
+builder.AddProject<Projects.Saver_BudgetService>("saver-budgetservice");
+
+builder.AddProject<Projects.Saver_FinanceService>("saver-financeservice");
+
+builder.AddProject<Projects.Saver_PredictionsService>("saver-predictionsservice");
+
+builder.AddProject<Projects.Saver_Client>("saver-client");
+
 builder.Build().Run();
