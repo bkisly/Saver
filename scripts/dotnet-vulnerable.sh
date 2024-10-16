a7aef4b9c1633645eae03af4f7d2266285ea549c
bkisly <b.kisly@gmail.com>
2024-10-16 09:46:39 +0200

    Added dotnet vulnerable script

---
+++ scripts/dotnet-vulnerable.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+dotnet list package --vulnerable 2>&1 | tee ./build.log
+echo "Analyzing log file for vulnerabilities..."
+grep -q -i "critical\|high" ./build.log
+rc=$?
+
+if [ $rc -eq 0 ]; then
+  exit 1
+elif [ $rc -eq 1 ]; then
+  exit 0
+else
+  exit $rc
+fi
