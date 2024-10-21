af7c17733131954ab4962bc269d8282c94555cc9
bkisly <b.kisly@gmail.com>
2024-10-21 19:21:59 +0200

    Implemented basic API for registration and authentication

--- src/Saver.IdentityService/Extensions/Extensions.cs
+++ src/Saver.IdentityService/Extensions/Extensions.cs
@@ -1,9 +1,36 @@
-﻿namespace Saver.IdentityService.Extensions;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Saver.IdentityService.Data;
+
+namespace Saver.IdentityService.Extensions;
 
 public static class Extensions
 {
-    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    public static IHostApplicationBuilder AddApplicationServices(this IHostApplicationBuilder builder)
     {
-        return services;
+        var services = builder.Services;
+
+        builder.AddNpgsqlDbContext<ApplicationDbContext>("identityservice-db");
+
+        services.AddIdentityApiEndpoints<IdentityUser>()
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+
+        services.AddAuthorization();
+        services.Configure<IdentityOptions>(options =>
+        {
+            options.SignIn.RequireConfirmedEmail = true;
+        });
+
+        builder.EnrichNpgsqlDbContext<ApplicationDbContext>();
+
+        return builder;
+    }
+
+    public static WebApplication ApplyMigrations(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        context.Database.Migrate();
+        return app;
     }
 }
--- src/Saver.IdentityService/Program.cs
+++ src/Saver.IdentityService/Program.cs
@@ -1,12 +1,23 @@
+using Saver.IdentityService.Api;
+using Saver.IdentityService.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 builder.AddDefaultSwagger();
+builder.AddApplicationServices();
 
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
 app.MapGet("/", () => "Hello World!");
+app.MapIdentityApiV1();
+
 app.UseDefaultSwagger();
+
+if (app.Environment.IsDevelopment())
+{
+    app.ApplyMigrations();
+}
 
 app.Run();
