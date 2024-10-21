0fcc81ece19d05b9ff38781130cf86b903c00660
bkisly <b.kisly@gmail.com>
2024-10-21 08:29:20 +0200

    Set up packages for identity

---
+++ src/Saver.IdentityService/Extensions/Extensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Saver.IdentityService.Extensions;
+
+public static class Extensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        return services;
+    }
+}
