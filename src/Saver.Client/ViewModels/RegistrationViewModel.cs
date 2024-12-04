072c74edb7d6abf7aedf5611def059575cf3c45d
bkisly <b.kisly@gmail.com>
2024-12-04 20:12:03 +0100

    Created basic form for registration

---
+++ src/Saver.Client/ViewModels/RegistrationViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Saver.Client.ViewModels;
+
+public class RegistrationViewModel
+{
+    [Required, EmailAddress] public string Email { get; set; } = string.Empty;
+    [Required] public string Password { get; set; } = string.Empty;
+    [Required] public string ConfirmPassword { get; set; } = string.Empty;
+}
