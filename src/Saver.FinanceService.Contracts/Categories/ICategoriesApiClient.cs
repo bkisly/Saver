872ef0f1537b63007f25d6be7218d48b7ccf6171
bkisly <b.kisly@gmail.com>
2024-12-04 10:57:38 +0100

    Created API clients for FinanceService

---
+++ src/Saver.FinanceService.Contracts/Categories/ICategoriesApiClient.cs
@@ -0,0 +1,21 @@
+﻿using Refit;
+
+namespace Saver.FinanceService.Contracts.Categories;
+
+public interface ICategoriesApiClient
+{
+    [Get("/api/finance/categories")]
+    Task<IEnumerable<CategoryDto>> GetCategoriesAsync();
+
+    [Get("/api/finance/categories/{id}")]
+    Task<ApiResponse<CategoryDto>> GetCategoryByIdAsync(Guid id);
+
+    [Post("/api/finance/categories")]
+    Task<HttpResponseMessage> CreateCategoryAsync([Body] CreateCategoryRequest request);
+
+    [Put("/api/finance/categories")]
+    Task<HttpResponseMessage> EditCategoryAsync([Body] EditCategoryRequest request);
+
+    [Delete("/api/finance/categories/{id}")]
+    Task<HttpResponseMessage> DeleteCategoryAsync(Guid id);
+}
