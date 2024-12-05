077dd4ccc3246945f624fc242ec712d1d1c445b9
bkisly <b.kisly@gmail.com>
2024-12-05 10:13:38 +0100

    Fixed chart

---
+++ src/Saver.Client/Components/Reports/BalanceChart.razor.js
@@ -0,0 +1,22 @@
+﻿function createChart() {
+    const ctx = document.getElementById('balance-chart');
+
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, -3, 5, 2, 3],
+                borderWidth: 3
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
